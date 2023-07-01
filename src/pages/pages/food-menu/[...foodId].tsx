a0fd692fcac2cd4a167d11e71f060ecc317dc025
Matthew Powell <matt.powell2784@gmail.com>
2023-07-01 21:09:49 +0100

    deploy test

--- src/pages/pages/food-menu/[...foodId].tsx
+++ src/pages/pages/food-menu/[...foodId].tsx
@@ -21,53 +21,53 @@ export default function FoodItemPage(foodItem: TSFoodItemResponse) {
   )
 }
 
-// export async function getStaticPaths() {
-//   try {
-//     const foodItemRequest = await apiCall({
-//       httpMethod: 'GET',
-//       route: 'api/v1/food-item/food-item',
-//     })
+export async function getStaticPaths() {
+  try {
+    const foodItemRequest = await apiCall({
+      httpMethod: 'GET',
+      route: '/api/v1/food-item/food-item',
+    })
 
-//     const paths = foodItemRequest.data.map((foodItem: any) => {
-//       return { params: { foodId: [foodItem.id.toString()] } }
-//     })
+    const paths = foodItemRequest.data.map((foodItem: any) => {
+      return { params: { foodId: [foodItem.id.toString()] } }
+    })
 
-//     return { paths, fallback: 'blocking' }
-//   } catch (error) {
-//     console.log(error)
-//     console.log('[...foodId] getStaticPaths catch block console log')
-//     return { paths: [], fallback: 'blocking' }
-//   }
-// }
+    return { paths, fallback: 'blocking' }
+  } catch (error) {
+    console.log(error)
+    console.log('[...foodId] getStaticPaths catch block console log')
+    return { paths: [], fallback: 'blocking' }
+  }
+}
 
-// export async function getStaticProps(context: GetStaticPropsContext) {
-//   try {
-//     const foodId = context.params?.foodId
-//     console.log('foodId', foodId)
-//     const foodItemRequest = await apiCall({
-//       httpMethod: 'GET',
-//       route: `/api/v1/food-item/${foodId}`,
-//     })
+export async function getStaticProps(context: GetStaticPropsContext) {
+  try {
+    const foodId = context.params?.foodId
+    console.log('foodId', foodId)
+    const foodItemRequest = await apiCall({
+      httpMethod: 'GET',
+      route: `/api/v1/food-item/${foodId}`,
+    })
 
-//     // const foodItemRequest = await (
-//     //   await fetch(
-//     //     `https://nextjs-food-take-away-app-xm8o.vercel.app/api/v1/food-item/${foodId}`
-//     //   )
-//     // ).json()
-//     // console.log('foodItemRequest', foodItemRequest)
+    // const foodItemRequest = await (
+    //   await fetch(
+    //     `https://nextjs-food-take-away-app-xm8o.vercel.app/api/v1/food-item/${foodId}`
+    //   )
+    // ).json()
+    // console.log('foodItemRequest', foodItemRequest)
 
-//     const foodItem = foodItemRequest.data.foodItem
-//     console.log('foodItem', foodItem)
+    const foodItem = foodItemRequest.data.foodItem
+    console.log('foodItem', foodItem)
 
-//     return {
-//       props: { foodItem },
-//       revalidate: 60,
-//     }
-//   } catch (error) {
-//     console.log(error)
-//     console.log('[...foodId] getStaticProps catch block console log')
-//     return {
-//       props: { foodItem: [] },
-//     }
-//   }
-// }
+    return {
+      props: { foodItem },
+      revalidate: 60,
+    }
+  } catch (error) {
+    console.log(error)
+    console.log('[...foodId] getStaticProps catch block console log')
+    return {
+      props: { foodItem: [] },
+    }
+  }
+}

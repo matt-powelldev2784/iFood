3cf817503de953030927156fa89de7c1652daa5f
Matthew Powell <matt.powell2784@gmail.com>
2023-07-01 20:07:35 +0100

    add error message to food id route

--- src/pages/api/v1/food-item/[...foodId].ts
+++ src/pages/api/v1/food-item/[...foodId].ts
@@ -39,14 +39,17 @@ const getFoodItemById = async (req: NextApiRequest, res: NextApiResponse) => {
   })
 
   if (!foodItem)
-    return res.status(400).json({
+    return res.status(500).json({
       success: false,
-      sdtatus: 500,
+      status: 500,
       errors: [
         {
           msg: 'Database Error. Food item not found.',
         },
       ],
+      error: {
+        msg: 'Database Error. Food item not found.',
+      },
     })
 
   return res.status(200).json({

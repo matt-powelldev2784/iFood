6a9f22273a7063814de6b69f06a4e31c249f26c1
Matthew Powell <matt.powell2784@gmail.com>
2023-07-21 14:29:37 +0100

    move food id route

---
+++ src/pages/food/[...foodId].tsx
@@ -0,0 +1,66 @@
+import { apiCall } from '@/utils/apiUtil'
+import { FoodPageItem, Navbar } from '@/components'
+import { GetStaticPropsContext } from 'next'
+import { TSFoodMenuItem } from '@/ts/interfaces'
+
+interface TSFoodItemResponse {
+  foodItem: TSFoodMenuItem
+}
+
+export default function FoodItemPage(foodItem: TSFoodItemResponse) {
+  const foodItemObj = foodItem.foodItem
+  console.log('foodItemObj', foodItemObj)
+
+  return (
+    <>
+      <title>Curry Club</title>
+      <Navbar />
+      <section className="flex min-h-screen items-start justify-center bg-quaternaryGrey/25">
+        <FoodPageItem foodItem={foodItemObj} />
+      </section>
+    </>
+  )
+}
+
+export async function getStaticPaths() {
+  try {
+    const foodItemRequest = await apiCall({
+      httpMethod: 'GET',
+      route: '/api/v1/food-item/food-item',
+    })
+
+    const paths = foodItemRequest.data.map((foodItem: any) => {
+      return { params: { foodId: [foodItem.id.toString()] } }
+    })
+
+    return { paths, fallback: 'blocking' }
+  } catch (error) {
+    // console.log(error)
+    console.log('[...foodId] getStaticPaths catch block console log')
+    return { paths: [], fallback: 'blocking' }
+  }
+}
+
+export async function getStaticProps(context: GetStaticPropsContext) {
+  try {
+    const foodId = context.params?.foodId
+
+    const foodItemRequest = await apiCall({
+      httpMethod: 'GET',
+      route: `/api/v1/food-item/${foodId}`,
+    })
+
+    const foodItem = foodItemRequest.data.foodItem
+
+    return {
+      props: { foodItem },
+      revalidate: 60,
+    }
+  } catch (error) {
+    // console.log(error)
+    console.log('[...foodId] getStaticProps catch block console log')
+    return {
+      props: { foodItem: [] },
+    }
+  }
+}
